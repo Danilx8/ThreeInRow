d564f7b1c05cb62039bd25eb50f085babb53d5ca
Danilx8 <lugovskihdanil@yandex.ru>
2025-08-15 20:11:27 +0700

    Добавил конструктор в учёт статистики

--- EventHandlers/StatisticsCounter.cs
+++ EventHandlers/StatisticsCounter.cs
@@ -4,6 +4,15 @@ namespace ThreeInRow.EventHandlers;
 
 public class StatisticsCounter
 {
+    private static StatisticsCounter? _statistics;
+    
+    private StatisticsCounter()
+    {
+        throw new NotImplementedException();
+    }
+
+    public StatisticsCounter Instance => _statistics ??= new StatisticsCounter();
+    
     public void AccountCombination()
     {
         throw new NotImplementedException();
